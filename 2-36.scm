0e919d39b8c25d11d2bb8a0b43d11c2937de3ace
Wang-Yann <wzy-511@163.com>
2016-05-04 02:11:25 +0800

    merge

--- 2-36.scm
+++ 2-36.scm
@@ -1,6 +1,7 @@
 (define (acc-n op int seqs)
 	(if (null? (car seqs))  '() 
 		(cons (acc op init  (map (lambda (subtree) ()) (car seqs)  )
-			(acc-n op init  ))))
+			(acc-n op init  (cdr seqs) ))))
 
 (load "filter.scm")
+
