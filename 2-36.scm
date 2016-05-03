b510ff5b811b70d4934f20e86364b84abcd5677b
Wang-Yann <wzy-511@163.com>
2016-05-03 11:00:13 +0800

    2-36xiugai

--- 2-36.scm
+++ 2-36.scm
@@ -1,6 +1,7 @@
-(define (acc-n op int seqs)
+(define (acc-n op init seqs)
 	(if (null? (car seqs))  '() 
-		(cons (acc op init  (map (lambda (subtree) ()) (car seqs)  )
-			(acc-n op init  ))))
+		(cons (acc op init  (map car seqs)  )
+			(acc-n op init (map cdr seqs )))))
 
 (load "filter.scm")
+(load "smplst.scm")
