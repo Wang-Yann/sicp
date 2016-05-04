41249ba979f6c777a28eaaefe28319e0b49b85e8
Wang-Yann <wzy-511@163.com>
2016-05-05 00:13:00 +0800

    2.3.2

---
+++ 2-54.scm
@@ -0,0 +1,10 @@
+(define (equal? x y)
+	(cond ((and (symbol? x) (symbol? y)) (eq? x y) )
+	((and (list? x) (list? y)) (list-equal? x y))
+	(else (error "wrong input " x y))))
+(define (list-equal? x y)
+	(cond( (and (null? x) (null? y)) #t)
+	((or (null? x) (null? y)) #f )
+	( (equal? (car x) (car y)) (equal? (cdr x) (cdr y)))
+	(else #f)))
+	
