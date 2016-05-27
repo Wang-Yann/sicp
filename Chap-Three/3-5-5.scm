519585cb06c38837c1f8a84763b51baab5ad1ede
Wang-Yann <wzy-511@163.com>
2016-05-27 23:06:25 +0800

    3-5-5

---
+++ Chap-Three/3-5-5.scm
@@ -0,0 +1,21 @@
+(load "3-5-3.scm")
+(define random-init 167)
+(define (rand-update x) (remainder (+ (* random-init  x) 12345) 183647))
+(define random-numbers
+	 (cons-stream random-init (stream-map rand-update random-numbers)))
+(define (map-successive-pairs f s)
+	(cons-stream
+		(f (stream-car s) (stream-car (stream-cdr s)))
+		(map-successive-pairs f (stream-cdr (stream-cdr s)))))
+(define cesaro-stream
+	 (map-successive-pairs (lambda (r1 r2) (= (gcd r1 r2) 1)) random-numbers))
+
+(define (monte-carlo experiment-stream passed failed)
+	(define (next passed failed)
+		(cons-stream (/ passed (+ passed failed))
+					(monte-carlo (stream-cdr experiment-stream) passed failed)))
+	(if (stream-car experiment-stream)
+		(next (+ passed 1) failed)
+		(next passed (+ failed 1))))
+(define PI1 (stream-map (lambda (p) (sqrt (/ 6 p))) (monte-carlo cesaro-stream  0 0)))
+;;(display-stream (stream-section PI1 10 20))
